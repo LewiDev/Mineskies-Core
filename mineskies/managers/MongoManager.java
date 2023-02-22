cd623595094d1e7342963c745f1df42428665ee8
TechLewi <lewi2437@gmail.com>
2023-02-22 23:41:01 +0000

    Update MongoManager.java

--- mineskies/managers/MongoManager.java
+++ mineskies/managers/MongoManager.java
@@ -1,12 +1,14 @@
 package me.lewi.dev.mineskies.managers;
 
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.ReplaceOptions;
 import me.lewi.dev.mineskies.Core;
 
 import me.lewi.dev.mineskies.storage.User;
 import org.bson.Document;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public class MongoManager {
 
@@ -16,24 +18,25 @@ public class MongoManager {
         this.core = core;
     }
 
-    public User load(UUID uuid, String name) {
-        Document document = (Document) core.getPlayers().find(Filters.eq("uuid", uuid)).first();
-        if (document == null) return new User(uuid, name);
-        User user = new User().load(document);
-        return user;
+    public CompletableFuture<User> load(UUID uuid, String name) {
+            return CompletableFuture.supplyAsync(() -> {
+                Document document = (Document) core.getPlayers().find(Filters.eq("uuid", uuid)).first();
+                if (document == null) return new User(uuid, name);
+                User user = new User().load(document);
+                return user;
+            });
     }
 
     public void save(User user) {
-        if(core.getPlayers().find(Filters.eq("uuid", user.getUuid())).first() == null) {
-            core.getPlayers().replaceOne(Filters.eq("uuid", user.getUuid()), user.save());
-            return;
-        }
-        core.getPlayers().insertOne(user.save());
-
+        CompletableFuture.runAsync(() -> {
+            core.getPlayers().replaceOne(Filters.eq("uuid", user.getUuid()), user.save(), new ReplaceOptions().upsert(true));
+        });
     }
 
     public void delete(User user) {
-        core.getPlayers().deleteOne(Filters.eq("uuid", user.getUuid()));
+        CompletableFuture.runAsync(() -> {
+            core.getPlayers().deleteOne(Filters.eq("uuid", user.getUuid()));
+        });
     }
 
 
